93ae68467ba0414fcf5ff0d3fec66fc03f1c38d6
Carlos <cp4213@gmail.com>
2026-02-05 19:49:26 -0500

    Update: +put +patch + Delete methods in gateway

--- lab2_e20/back-end-cloud-gateway-filters-master/src/main/java/com/unir/gateway/decorator/PutRequestDecorator.java
+++ lab2_e20/back-end-cloud-gateway-filters-master/src/main/java/com/unir/gateway/decorator/PutRequestDecorator.java
@@ -17,71 +17,38 @@ import reactor.core.publisher.Flux;
 
 import java.net.URI;
 
-/**
- * This class is a decorator for the GatewayRequest object for POST requests.
- * It extends the ServerHttpRequestDecorator class and overrides its methods to modify the request.
- * It uses the ObjectMapper to convert the body of the GatewayRequest object into bytes.
- */
 @Slf4j
-public class PostRequestDecorator extends ServerHttpRequestDecorator {
+public class PutRequestDecorator extends ServerHttpRequestDecorator {
 
     private final GatewayRequest gatewayRequest;
     private final ObjectMapper objectMapper;
 
-    public PostRequestDecorator(GatewayRequest gatewayRequest, ObjectMapper objectMapper) {
+    public PutRequestDecorator(GatewayRequest gatewayRequest, ObjectMapper objectMapper) {
         super(gatewayRequest.getExchange().getRequest());
         this.gatewayRequest = gatewayRequest;
         this.objectMapper = objectMapper;
     }
 
-    /**
-     * This method overrides the getMethod method of the ServerHttpRequestDecorator class.
-     * It returns the HTTP method of the request, which is POST.
-     *
-     * @return the HTTP method of the request
-     */
     @Override
     @NonNull
-    public HttpMethod getMethod() {
-        return HttpMethod.POST;
-    }
+    public HttpMethod getMethod() {return HttpMethod.PUT;}
 
-    /**
-     * This method overrides the getURI method of the ServerHttpRequestDecorator class.
-     * It returns the URI of the request.
-     *
-     * @return the URI of the request
-     */
     @Override
     @NonNull
     public URI getURI() {
         return UriComponentsBuilder
-                .fromUri((URI)
-                        gatewayRequest
+                .fromUri(
+                        (URI) gatewayRequest
                                 .getExchange()
                                 .getAttributes()
                                 .get(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR)
                 ).build().toUri();
     }
 
-    /**
-     * This method overrides the getHeaders method of the ServerHttpRequestDecorator class.
-     * It returns the headers of the request.
-     *
-     * @return the headers of the request
-     */
     @Override
     @NonNull
-    public HttpHeaders getHeaders() {
-        return gatewayRequest.getHeaders();
-    }
+    public HttpHeaders getHeaders() {return gatewayRequest.getHeaders();}
 
-    /**
-     * This method overrides the getBody method of the ServerHttpRequestDecorator class.
-     * It converts the body of the GatewayRequest object into bytes using the ObjectMapper, and returns it as a Flux of DataBuffers.
-     *
-     * @return a Flux of DataBuffers representing the body of the request
-     */
     @Override
     @NonNull
     @SneakyThrows
@@ -93,3 +60,4 @@ public class PostRequestDecorator extends ServerHttpRequestDecorator {
         return Flux.just(buffer);
     }
 }
+
